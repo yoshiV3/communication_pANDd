1b4c8b10981f3c7c3a9cce58b1a2a05f24348d44
yoshiV3 <yoshi.vermeire@student.kuleuven.be>
2020-05-15 19:30:54 +0200

    better feedback about results

--- feedback.py
+++ feedback.py
@@ -5,10 +5,12 @@ interface = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 interface.bind(('192.168.0.1', 9991))
 testing = True
 def receive():
+        number =  0
         while testing:
+                number = number + 1
                 recv, addr = interface.recvfrom(1024)
                 recv = list(recv)
-                print(str(recv))
+                print(str(number) +": " + str(recv))
 threading.Thread(target=receive).start()
 
 
