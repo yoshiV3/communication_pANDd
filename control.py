465f093a47750ff531675bddc1281a8cd60ae796
yoshiV3 <yoshi.vermeire@student.kuleuven.be>
2020-05-15 19:33:35 +0200

    another test

--- control.py
+++ control.py
@@ -5,7 +5,7 @@ interface = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 interface.bind(('192.168.0.2', 9990))
 addr = ("192.168.0.2", 9991)
 testing = True 
-msg = bytes([4,4,4,4])
+msg = bytes([4,4,4,4]*50)
 for i in range(20):
 	interface.sendto(msg, addr)
 	time.sleep(1)
